251ad1c68ace17c3f440d23c6117a71d70b50b56
Milene Toazza <oi@mihtoa.ppg.br>
2018-09-09 20:20:04 -0300

    Add back to front

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name='Sobre' />
+    <h3>Nossa História</h3>
+    <p>Lalala</p>
+    <h3>Missão</h3>
+    <p>Lalala</p>
+    <h3>Visão</h3>
+    <p>Lalala</p>
+  </div>
+)
